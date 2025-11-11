8f57f6a1e0b2f59bd2e51614985daf5ca85c6e6c
Brendo <brendowashingtone@gmail.com>
2025-11-11 12:38:36 -0300

    Deixando admModel com 100% de test coverage. Adicionado teste para retornar os aniversariantes

--- src/models/membrosModel.js
+++ src/models/membrosModel.js
@@ -7,10 +7,6 @@ class membrosModel {
     return membroCadastro.insertId;
   }
 
-
-
-
-
   async cadastrarEndereco(membros_id, endereco, connection) {
     const [membroEndereco] = await connection.query(
       'INSERT INTO membros_endereco (estado, cidade, bairro, rua, complemento, membros_id) VALUES (?, ?, ?, ?, ?, ?)',
@@ -27,10 +23,6 @@ class membrosModel {
     return membroEndereco.insertId;
   }
 
-
-
-
-
   async cadastrarTelefone(membros_id, telefone, connection) {
     const [membroTelefone] = await connection.query(
       'INSERT INTO membros_telefone (telefone, membros_id) VALUES (?, ?)',
@@ -40,11 +32,9 @@ class membrosModel {
     return membroTelefone.insertId;
   }
 
-
-
-
   async buscarMembros(url, connection) {
-    const [membros] = await connection.query(`SELECT
+    const [membros] = await connection.query(
+      `SELECT
     m.id,
     m.nome,
     m.nascimento,
@@ -65,13 +55,16 @@ FROM
         JOIN
     membros_telefone mt ON mt.membros_id = m.id
 WHERE
-    url = ? `, [url]);
+    url = ? `,
+      [url]
+    );
 
     return membros;
   }
 
-   async buscarMembro(url, id, connection) {
-    const [membro] = await connection.query(`SELECT
+  async buscarMembro(url, id, connection) {
+    const [membro] = await connection.query(
+      `SELECT
     m.id,
     m.nome,
     m.nascimento,
@@ -92,14 +85,16 @@ FROM
         JOIN
     membros_telefone mt ON mt.membros_id = m.id
 WHERE
-    url = ? AND m.id = ?`, [url, id]);
+    url = ? AND m.id = ?`,
+      [url, id]
+    );
 
     return membro;
   }
 
-
-  async buscarAniversariantes (url, mes, connection) {
-    const [aniversariantes] = await connection.query(`SELECT
+  async buscarAniversariantes(url, mes, connection) {
+    const [aniversariantes] = await connection.query(
+      `SELECT
     m.id,
     m.nome,
     m.nascimento,
@@ -111,11 +106,12 @@ FROM
         JOIN
     membros_telefone mt ON mt.membros_id = m.id
 WHERE
-    url = ? AND MONTH (m.nascimento) = ?`, [url, mes]);
+    url = ? AND MONTH (m.nascimento) = ?`,
+      [url, mes]
+    );
 
     return aniversariantes;
   }
-
 }
 
 export default new membrosModel();
